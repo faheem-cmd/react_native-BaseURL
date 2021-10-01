6b274402ec48efbdc4973153df36abf64212855a
Muhammed Faheem <56709898+faheem-cmd@users.noreply.github.com>
2021-10-01 11:56:54 +0530

    demo

---
+++ Global.js
@@ -0,0 +1,26 @@
+
+const baseUrl = "https://www.demo.in/"
+
+export default {baseUrl}
+
+
+//other pages
+
+import global from './Global'
+url: global.baseUrl + 'api/auth/login/',
+
+
+//passing data through params
+
+ onPress={() => navigation.navigate('Content', { item: data })}>
+
+  #data= mapping array
+  item = data
+
+
+//next page
+
+
+ ({ navigation, route }) 
+
+ <Text>{route.params.item.name}</Text>
